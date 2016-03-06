c14ac16c9c84090abe57b5690d7390c0c61e6eaa
Adria Rios <adrianrios89@gmail.com>
2016-03-06 11:45:32 +0100

    Added Java doc to TweetsStoragedManager

--- app/src/main/java/org/adriarios/hourtweets/hourtweets/data/storage/TweetsStoragedManager.java
+++ app/src/main/java/org/adriarios/hourtweets/hourtweets/data/storage/TweetsStoragedManager.java
@@ -9,16 +9,28 @@ import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
 
 /**
- * Created by Adrian on 04/03/2016.
+ * This is the Tweets Storaged Manager. His goal to persist Tweets in the local storage of the
+ * device and serve this tweets. The database selected for this task is Realm.
+ *
+ * Created by Adrian on 03/03/2016.
  */
 public class TweetsStoragedManager implements ITweetsStoragedManager {
     private Realm realm;
 
+    /**
+     * Contructor of the class. Initializes the Realm Instance.
+     * @param application
+     */
     public TweetsStoragedManager(App application) {
         RealmConfiguration realmConfig = new RealmConfiguration.Builder(application).build();
         realm = Realm.getInstance(realmConfig);
     }
 
+    /**
+     * This method return a stored tweet by provided id (the hour of the tweet)
+     * @param currentTweetHour Hour of the tweet, used as id to make the query
+     * @return
+     */
     public TweetRealmObjectVO getStoragedTweet(String currentTweetHour) {
         TweetRealmObjectVO tweet = null;
         RealmResults<TweetRealmObjectVO> tweetResult = realm.where(TweetRealmObjectVO.class)
@@ -31,19 +43,11 @@ public class TweetsStoragedManager implements ITweetsStoragedManager {
         return tweet;
     }
 
-    private void deleteIfExists(String id) {
-        RealmResults<TweetRealmObjectVO> tweetResult = realm.where(TweetRealmObjectVO.class)
-                .equalTo("id", id)
-                .findAll();
-        // All changes to data must happen in a transaction
-        realm.beginTransaction();
-        // remove single match
-        if (tweetResult.size() > 0) {
-            tweetResult.remove(0);
-        }
-        realm.commitTransaction();
-    }
-
+    /**
+     * This method stores a new Tweet in local storage
+     * @param tweet Tweet to store
+     * @param currentTweetHour Hour of the tweet, used as id
+     */
     public void addNewTweetToLocalStorage(Tweet tweet, String currentTweetHour) {
         deleteIfExists(currentTweetHour);
         TweetRealmObjectVO tweetRealm = new TweetRealmObjectVO();
@@ -56,6 +60,23 @@ public class TweetsStoragedManager implements ITweetsStoragedManager {
 
         realm.beginTransaction();
         realm.copyToRealm(tweetRealm);
+        realm.commitTransaction();
+    }
+
+    /**
+     * This method deletes a new Tweet from the local storage
+     * @param id Id to tweet to be deleted
+     */
+    private void deleteIfExists(String id) {
+        RealmResults<TweetRealmObjectVO> tweetResult = realm.where(TweetRealmObjectVO.class)
+                .equalTo("id", id)
+                .findAll();
+        // All changes to data must happen in a transaction
+        realm.beginTransaction();
+        // remove single match
+        if (tweetResult.size() > 0) {
+            tweetResult.remove(0);
+        }
         realm.commitTransaction();
     }
 }
